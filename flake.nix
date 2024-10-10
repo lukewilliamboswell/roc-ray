f078a340c9a4473ce08d25a7c2098bde360e9e33
Luke Boswell <lukewilliamboswell@gmail.com>
2024-10-10 15:01:09 +1100

    try add linux deps to nix

--- flake.nix
+++ flake.nix
@@ -14,9 +14,24 @@
         devShells = forAllSystems (system:
         let
             pkgs = nixpkgs.legacyPackages.${system};
+
             rocPkgs = roc.packages.${system};
+
+            linuxDeps = if pkgs.stdenv.isLinux then [
+                pkgs.libasound
+                pkgs.libX11
+                pkgs.libXrandr
+                pkgs.libXi
+                pkgs.mesa
+                pkgs.libGLU
+                pkgs.libXcursor
+                pkgs.libXinerama
+                pkgs.wayland
+                pkgs.libxkbcommon
+            ] else [];
+
             macosDeps = if pkgs.stdenv.isDarwin then [
-            pkgs.darwin.apple_sdk.frameworks.Foundation
+                pkgs.darwin.apple_sdk.frameworks.Foundation
                 pkgs.darwin.apple_sdk.frameworks.CoreServices
                 pkgs.darwin.apple_sdk.frameworks.CoreGraphics
                 pkgs.darwin.apple_sdk.frameworks.AppKit
@@ -24,22 +39,24 @@
                 pkgs.darwin.apple_sdk.frameworks.AudioToolbox
                 pkgs.darwin.apple_sdk.frameworks.CoreMIDI
             ] else [];
+
         in {
             default = pkgs.mkShell {
-            packages = [ pkgs.zig_0_13  rocPkgs.cli ] ++ macosDeps;
-            shellHook = ''
-                if [ "$(uname)" = "Darwin" ]; then
-                    export SDKROOT=$(xcrun --show-sdk-path)
-                fi
 
-                # We unset some NIX environment variables that might interfere with the zig
-                # compiler.
-                # Issue: https://github.com/ziglang/zig/issues/18998
-                unset NIX_CFLAGS_COMPILE
-                unset NIX_LDFLAGS
-            '';
+                packages = [ pkgs.zig_0_13  rocPkgs.cli ] ++ linuxDeps ++ macosDeps;
+
+                shellHook = ''
+                    if [ "$(uname)" = "Darwin" ]; then
+                        export SDKROOT=$(xcrun --show-sdk-path)
+                    fi
+
+                    # We unset some NIX environment variables that might interfere with the zig
+                    # compiler.
+                    # Issue: https://github.com/ziglang/zig/issues/18998
+                    unset NIX_CFLAGS_COMPILE
+                    unset NIX_LDFLAGS
+                '';
             };
-        }
-        );
+        });
     };
 }

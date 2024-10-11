ec3e4f9898494f15c57d89d74205097c30e490b4
Luke Boswell <lukewilliamboswell@gmail.com>
2024-10-11 12:05:56 +1100

    have zig drive build to simplify linking

---
+++ build-and-run.ps1
@@ -0,0 +1,9 @@
+﻿
+# build host
+C:\zig-windows-x86_64-0.13.0\zig.exe build --release=fast
+
+# run the executable
+.\zig-out\bin\rocray.exe
+
+# bundle the host with raylib (NOT USED... leaving this here for a future where we try to prebuild the host)
+#LIB.EXE /OUT:./platform/windows-x64.lib /VERBOSE /LTCG .\zig-out\lib\raylib.lib .\zig-out\lib\rocray.lib
--- build.zig
+++ build.zig
@@ -12,12 +12,41 @@ pub fn build(b: *std.Build) !void {
     // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
+    const roc_src = b.option([]const u8, "app", "the roc application to build");
+
+    const build_roc = b.addExecutable(.{
+        .name = "build_roc",
+        .root_source_file = b.path("build_roc.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    const run_build_roc = b.addRunArtifact(build_roc);
+    // By setting this to true, we ensure zig always rebuilds the roc app since it can't tell if any transitive dependencies have changed.
+    run_build_roc.stdio = .inherit;
+    run_build_roc.has_side_effects = true;
+
+    if (roc_src) |val| {
+        run_build_roc.addFileArg(b.path(val));
+    } else {
+        run_build_roc.addFileArg(b.path("examples/basic-shapes.roc"));
+    }
+
+    switch (optimize) {
+        .ReleaseFast, .ReleaseSafe => {
+            run_build_roc.addArg("--optimize");
+        },
+        .ReleaseSmall => {
+            run_build_roc.addArg("--opt-size");
+        },
+        else => {},
+    }
+
     const raylib_dep = b.dependency("raylib-zig", .{
         .target = target,
         .optimize = optimize,
     });
 
-    const lib = b.addStaticLibrary(.{
+    const exe = b.addExecutable(.{
         .name = "rocray",
         .root_source_file = b.path("host/main.zig"),
         .target = target,
@@ -28,10 +57,13 @@ pub fn build(b: *std.Build) !void {
     const raygui = raylib_dep.module("raygui"); // raygui module
     const raylib_artifact = raylib_dep.artifact("raylib"); // raylib C library
 
-    lib.linkLibrary(raylib_artifact);
-    lib.root_module.addImport("raylib", raylib);
-    lib.root_module.addImport("raygui", raygui);
+    exe.step.dependOn(&run_build_roc.step);
 
-    b.installArtifact(lib);
-    b.installArtifact(raylib_artifact);
+    exe.addObjectFile(b.path("app.o"));
+    exe.linkLibrary(raylib_artifact);
+
+    exe.root_module.addImport("raylib", raylib);
+    exe.root_module.addImport("raygui", raygui);
+
+    b.installArtifact(exe);
 }

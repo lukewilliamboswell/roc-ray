59faec84e2c03a061e059ce4f1915941666c7faf
Luke Boswell <lukewilliamboswell@gmail.com>
2024-10-16 17:08:33 +1100

    build script for linux

--- src/bindings/mod.rs
+++ src/bindings/mod.rs
@@ -5,3 +5,11 @@ mod aarch64_apple_darwin;
 #[cfg(target_arch = "aarch64")]
 #[cfg(target_os = "macos")]
 pub use aarch64_apple_darwin::*;
+
+#[cfg(target_arch = "x86_64")]
+#[cfg(target_os = "linux")]
+mod x86_64_unknown_linux_gnu;
+
+#[cfg(target_arch = "x86_64")]
+#[cfg(target_os = "linux")]
+pub use x86_64_unknown_linux_gnu::*;

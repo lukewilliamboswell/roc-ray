2c0dd73d2daba9c46e5fa2a137c17f5516af6a31
Luke Boswell <lukewilliamboswell@gmail.com>
2024-11-08 18:29:42 +1100

    use roc from installed cache

--- windows/setup.ps1
+++ windows/setup.ps1
@@ -1,5 +1,5 @@
 $url = "https://github.com/lukewilliamboswell/roc/releases/download/windows-20241108/roc.exe"
-$binDir = "$PSScriptRoot\bin\" 
+$binDir = "$PSScriptRoot\bin\"
 $path = "$binDir\roc.exe"
 
 # download roc to /windows/bin
@@ -7,18 +7,4 @@ if (!(Test-Path $path)) {
   New-Item -ItemType Directory -Force -Path $binDir
   $wc = New-Object System.Net.WebClient
   $wc.DownloadFile($url, $path)
-}
-
-# Add to PATH if not already present
-$currentPath = [Environment]::GetEnvironmentVariable("Path", "User")
-if ($currentPath -notlike "*$binDir*") {
-    $newPath = $currentPath + ";" + $binDir
-    [Environment]::SetEnvironmentVariable("Path", $newPath, "User")
-
-    # Update current session's PATH as well
-    $env:Path = $newPath
-
-    Write-Host "Added $binDir to PATH"
-} else {
-    Write-Host "$binDir is already in PATH"
 }
